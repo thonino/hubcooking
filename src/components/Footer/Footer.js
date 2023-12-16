56de9440ff68ffb212b4c0a3511207c6f30654ba
thonino <thoninoben@gmail.com>
2023-12-16 02:53:17 +0100

    footed fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className="roboto ">
-      <footer className="bg-red text-white p-1 sm:fixed sm:inset-x-0 sm:bottom-0">
+    <div className="roboto footer-fix">
+      <footer className="bg-red text-white p-1 fixed inset-x-0 bottom-0 ">
         <div className="flex flex-col sm:flex-row justify-between items-center">
           <div>
             <p className="text-white text-lg font-bold ml-2">
               HubCooking.fr
+          <Link to="/" className="hover:underline italic font-light mx-2">Politique de confidentialité</Link>
             </p>
           </div>
-          <Link to="/" className="hover:underline italic">Contact</Link>
-          <Link to="/" className="hover:underline italic">Politique de confidentialité</Link>
-          <div className="flex flex-row mr-2">
+          <div className="flex flex-row mx-2">
+          <Link to="/contact" className="hover:underline italic mx-2">Contact</Link>
               &copy; {new Date().getFullYear()} Tous droits réservés.
           </div>
         </div>
