9c4b3c649b5e8f503d92d3bc3c558dbc1805161d
thonino <thoninoben@gmail.com>
2023-12-25 23:54:08 +0100

    addPicture : fixed

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
@@ -6,11 +6,12 @@ function Categories() {
   const [pictures, setPictures] = useState([]);
   const [categories, setCategories] = useState([]);
   const navigate = useNavigate();
-  const { name } = useParams(); 
-  const [updateCategory, setUpdateCategory] = useState();
+  const { name } = useParams();
   const [updateName, setUpdateName] = useState();
   const [updateImg, setUpdateImg] = useState();
-  
+  const [updateCategory, setUpdateCategory] = useState("");
+  const [filter, setFilter] = useState([]);
+
   useEffect(() => {
     fetch("/api/categories")
       .then((response) => response.json())
@@ -23,20 +24,20 @@ function Categories() {
       .then((json) => setPictures(json.pictures));
   }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const id = pictures.length;
-    const name = e.target.itemName.value;
-    const img = e.target.fileName.value;
-    const user_id = 0;
-    const like = 0;
-    const newData = { id, name, img, user_id, like };
-    name && setPictures([...pictures, newData]);
-  };
+  useEffect(() => {
+    if (name) {
+      const filteredPictures = pictures.filter(
+        (data) => data.category === name
+      );
+      setFilter(filteredPictures);
+    } else {
+      setFilter(pictures); // Affiche toutes les images si aucun paramètre de catégorie n'est spécifié
+    }
+  }, [name, pictures]);
 
   const handleDelete = (data) => {
-    const newDatas = pictures.filter((item) => item.id !== data.id);
-    setPictures(newDatas);
+    const newDatas = filter.filter((item) => item.id !== data.id);
+    setFilter(newDatas);
   };
 
   const handleUpdate = (data) => {
@@ -50,10 +51,10 @@ function Categories() {
     if (updateImg) {
       updateData.img = updateImg;
     }
-    const newDatas = pictures.map((item) =>
+    const newDatas = filter.map((item) =>
       item.id === data.id ? { ...item, ...updateData } : item
     );
-    setPictures(newDatas);
+    setFilter(newDatas);
   };
 
   const handleToggle = (retrieve) => {
@@ -64,15 +65,29 @@ function Categories() {
     });
   };
 
-  const catFilter = pictures.filter((data) => data.category === name);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = filter.length;
+    const name = e.target.itemName.value;
+    const img = e.target.fileName.value;
+    const user_id = 0;
+    const like = 0;
+    const newData = { id, name, img, user_id, like };
+    if (name) {
+      setFilter([...filter, newData]);
+    }
+  };
 
   return (
     <div>
-      <h1 className="hc-blue pacifico text-3xl mt-2">Trier categorie</h1>
+      <h1 className="hc-blue pacifico text-3xl mt-2">
+        Trier par catégorie : {name}
+      </h1>
       <br />
       <select
         className="border rounded p-2 mb-2 bg-red-light w200 text-center"
         onChange={(e) => navigate(`/categories/${e.target.value}`)}
+        value={name}
       >
         <option value="">Catégorie</option>
         {categories.map((data) => (
@@ -81,7 +96,7 @@ function Categories() {
           </option>
         ))}
       </select>
-      <br/>
+      <br />
       <button
         className="btn btn-green bg-green mb-2 mr-2"
         onClick={() => handleToggle("toggleAdd")}
@@ -102,7 +117,7 @@ function Categories() {
       </button>
 
       <div className="flex flex-col justify-center items-center">
-        <form onSubmit={handleSubmit} className="toggleAdd ">
+        <form onSubmit={handleSubmit} className="toggleAdd">
           <h1 className="hc-blue pacifico text-xl my-3">Publiez une photo !</h1>
           <div className="flex flex-col gap-y-2">
             <input
@@ -117,7 +132,11 @@ function Categories() {
               className="border rounded p-2"
               placeholder="nom fichier (.png)"
             />
-            <select className="border rounded p-2">
+            <select
+              className="border rounded p-2"
+              value={updateCategory}
+              onChange={(e) => setUpdateCategory(e.target.value)}
+            >
               {categories.map((data) => (
                 <option key={data.id} value={data.name}>
                   {data.name}
@@ -131,9 +150,9 @@ function Categories() {
         </form>
       </div>
 
-      <div className=" ">
-        <div className="flex flex-wrap justify-center ">
-          {catFilter.map((data) => (
+      <div>
+        <div className="flex flex-wrap justify-center">
+          {filter.map((data) => (
             <div key={data.id} className="m-1 w-img flex flex-col">
               <div className={"toggleEdit"}>
                 <input
