4ccc1a04bfacb993a84e62016e04c47a98a8bbc4
thonino <thoninoben@gmail.com>
2023-12-29 20:57:27 +0100

    make recipe

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -14,18 +14,18 @@ function Recipes() {
 
 
   return (
-  <div>
-    <h1>Les Recettes</h1>
-    <br />
-    <div className="flex flex-wrap justify-center ">
-      {recipes.map((data) => (
-        <div key={data.id} className="m-1 w-img flex flex-col">
-          {data.name}
-          <img className="rounded" src={"/img/" + data.img} alt={data.name} />
-        </div>
-      ))}
-  </div>
-  </div>
+    <div>
+      <h1 className="hc-blue pacifico text-3xl mt-2">Toutes Les Recettes</h1>
+      <br />
+      <div className="flex flex-wrap justify-center ">
+        {recipes.map((data) => (
+          <div key={data.id} className="m-1 w-img flex flex-col">
+            {data.name}
+            <img className="rounded" src={"/img/" + data.img} alt={data.name} />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
